44e97c7c7e3c4981f70bf1c0e75f1592003dad90
DigKleppe <Digkleppe@gmail.com>
2025-06-27 16:15:47 +0200

    spiffs updated

--- spiffs_image/js/chart.js
+++ spiffs_image/js/chart.js
@@ -21,18 +21,34 @@ var LOGDAYS = 1;
 var MAXPOINTS = (LOGDAYS * 24 * 60 * 60 / SECONDSPERTICK)
 
 var dayNames = ['zo', 'ma', 'di', 'wo', 'do', 'vr', 'za'];
-var displayNames = ["", "1", "2", "3", "4"];
-var cbIDs = ["", "T1cb", "T2cb", "T3cb", "T4cb"];
+var displayNames = ["", "S1", "S2", "S3", "S4"];
+var cbIDs = ["", "S1cb", "S2cb", "S3cb", "S4cb"];
 
 var chartSeries = [-1, -1, -1, -1, -1];
 
-var NRItems = displayNames.length;
+var NRSensors = displayNames.length;
+var NRFields = 4; // timestamp, co2, t , rh 
 
-
-var chartOptions = {
+var CO2Options = {
 	title: '',
 	curveType: 'function',
-	legend: { position: 'bottom' },
+	legend: { position: 'top' },
+	heigth: 200,
+	crosshair: { trigger: 'both' },	// Display crosshairs on focus and selection.
+	explorer: {
+		actions: ['dragToZoom', 'rightClickToReset'],
+		//actions: ['dragToPan', 'rightClickToReset'],
+		axis: 'horizontal',
+		keepInBounds: true,
+		maxZoomIn: 100.0
+	},
+	chartArea: { 'width': '90%', 'height': '80%' },
+};
+
+var tempAndRHoptions = {
+	title: '',
+	curveType: 'function',
+	legend: { position: 'top' },
 
 	heigth: 200,
 	crosshair: { trigger: 'both' },	// Display crosshairs on focus and selection.
@@ -43,187 +59,140 @@ var chartOptions = {
 		keepInBounds: true,
 		maxZoomIn: 100.0
 	},
-	chartArea: { 'width': '90%', 'height': '60%' },
+	chartArea: { 'width': '90%', 'height': '80%' },
 
 	vAxes: {
 		0: { logScale: false },
+		1: { logScale: false }
+	},
+	series: {
+		0: { targetAxisIndex: 0 },// temperature
+		1: { targetAxisIndex: 1 },// RH
 	},
 };
 
 function clear() {
-	data.removeRows(0, data.getNumberOfRows());
-	chart.draw(data, chartOptions);
+	tempAndRHdata.removeRows(0, tempAndRHdata.getNumberOfRows());
+	CO2Data.removeRows(0, CO2Data.getNumberOfRows());
+	tRHchart.draw(tempAndRHdata, tempAndRHoptions);
+	CO2chart.draw(CO2Data, CO2Options);
 	tick = 0;
 }
 
 //var formatter_time= new google.visualization.DateFormat({formatType: 'long'});
 // channel 1 .. 5
 
-function plot(channel, value) {
-	if (chartRdy) {
-		if (channel == 1) {
-			data.addRow();
-			if (data.getNumberOfRows() > MAXPOINTS == true)
-				data.removeRows(0, data.getNumberOfRows() - MAXPOINTS);
-		}
-		if (value != '--') {
-			if (value > -50.0) {
-				value = parseFloat(value); // from string to float
-				data.setValue(data.getNumberOfRows() - 1, channel, value);
-			}
-		}
-	}
+function initChart() {
+	window.addEventListener('load', loadCBs());
 }
 
 function loadCBs() {
 	var cbstate;
-
 	console.log("Reading CBs");
-
 	// Get the current state from localstorage
 	// State is stored as a JSON string
 	cbstate = JSON.parse(localStorage['CBState'] || '{}');
-
 	// Loop through state array and restore checked 
 	// state for matching elements
 	for (var i in cbstate) {
 		var el = document.querySelector('input[name="' + i + '"]');
 		if (el) el.checked = true;
 	}
-
 	// Get all checkboxes that you want to monitor state for
 	var cb = document.getElementsByClassName('save-cb-state');
-
 	// Loop through results and ...
 	for (var i = 0; i < cb.length; i++) {
-
 		//bind click event handler
-		cb[i].addEventListener('click', function(evt) {
+		cb[i].addEventListener('click', function (evt) {
 			// If checkboxe is checked then save to state
 			if (this.checked) {
 				cbstate[this.name] = true;
 			}
-
 			// Else remove from state
 			else if (cbstate[this.name]) {
 				delete cbstate[this.name];
 			}
-
 			// Persist state
 			localStorage.CBState = JSON.stringify(cbstate);
 		});
 	}
 	console.log("CBs read");
-	initTimer();
+	initChart2();
 };
 
+function initChart2() {
+	CO2chart = new google.visualization.LineChart(document.getElementById('CO2chart'));
+	CO2Data = new google.visualization.DataTable();
+	CO2Data.addColumn('string', 'Time');
+	tRHchart = new google.visualization.LineChart(document.getElementById('tRHchart'));
+	tempAndRHdata = new google.visualization.DataTable();
+	tempAndRHdata.addColumn('string', 'Time');
 
-function initChart() {
-	window.addEventListener('load', loadCBs());
-
-}
-
-function initTimer() {
-	var activeSeries = 1;
-	chart = new google.visualization.LineChart(document.getElementById('chart'));
-	data = new google.visualization.DataTable();
-	data.addColumn('string', 'Time');
-
-	for (var m = 1; m < NRItems; m++) { // time not used for now 
+	for (var m = 1; m < NRSensors; m++) {
 		var cb = document.getElementById(cbIDs[m]);
 		if (cb) {
 			if (cb.checked) {
-				data.addColumn('number', displayNames[m]);
+				CO2Data.addColumn('number', displayNames[m] + ":CO2");
+				tempAndRHdata.addColumn('number', displayNames[m] + ':t');
+				tempAndRHdata.addColumn('number', displayNames[m] + ':RH');
 				chartSeries[m] = activeSeries;
 				activeSeries++;
 			}
 		}
 	}
-	if (activeSeries == 1) { // then none selected, select first
-		var cb = document.getElementById(cbIDs[1]);
-		data.addColumn('number', displayNames[1]);
-		chartSeries[1] = 1;
-		cb.checked = true;
-	}
+	//SIMULATE = true;	
+	startTimer();
+}
 
-	chartRdy = true;
-	makeChargeInfoTable();
-	if (SIMULATE) {
-		simplot();
+function plot(channel, co2, t, rh, timeStamp) {
+	tempAndRHdata.addRow();
+	CO2data.addRow();
+	if (CO2data.getNumberOfRows() > MAXPOINTS == true) {
+		CO2data.removeRows(0, CO2data.getNumberOfRows() - MAXPOINTS);
+		tempAndRHdata.removeRows(0, CO2data.getNumberOfRows() - MAXPOINTS);
 	}
-	else {
-		setInterval(function() { timer() }, 1000);
+	value = parseFloat(co2);
+	CO2data.setValue(CO2data.getNumberOfRows() - 1, channel, value);
+	value = parseFloat(t);
+	tempAndRHdata.setValue(tempAndRHdata.getNumberOfRows() - 1, channel, value);
+	value = parseFloat(rh);
+	tempAndRHdata.setValue(tempAndRHdata.getNumberOfRows() - 1, channel * 2, value);
+	if (channel == 1) {
+		var date = new Date(timeStamp);
+		var labelText = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+		CO2data.setValue(chartData.getNumberOfRows() - 1, 0, labelText);
+		tempAndRHdata.setValue(chartData.getNumberOfRows() - 1, 0, labelText);
 	}
 }
 
-
-function updateLastDayTimeLabel(data) {
-	var ms = Date.now();
-	var date = new Date(ms);
-	var labelText = date.getHours() + ':' + date.getMinutes();
-	data.setValue(data.getNumberOfRows() - 1, 0, labelText);
-}
-
-function updateAllDayTimeLabels(data) {
-	var rows = data.getNumberOfRows();
-	var secondsAgo = rows * SECONDSPERTICK;
-	var ms = Date.now();
-	ms -= (secondsAgo * 1000);
-	for (var n = 0; n < rows; n++) {
-		var date = new Date(ms);
-		var labelText = dayNames[date.getDay()] + ';' + date.getHours() + ':' + date.getMinutes();
-		data.setValue(n, 0, labelText);
-		ms += 1000 * SECONDSPERTICK;
-	}
-}
-
-function simplot() {
-	var nrLines;
-
-	if (firstRequest) {
-		nrLines = 50;
-		firstRequest = false;
-	}
-	else
-		nrLines = 1;
-	var str = '';
-	for (var m = 0; m < nrLines; m++) {
-		tick++;
-		str = str + tick + ',';
-		for (var n = 0; n < NRItems; n++) {  // voltages
-			simValue1 += 0.1;
-			simValue2 = 1 + Math.sin(simValue1) * 0.4;
-			str = str + simValue2;
-			if (n < (NRItems - 1))
-				str = str + ',';
-			else
-				str = str + '\n';
-		}
-	}
-	plotArray(str);
-}
-
-function plotArray(str) {
+// 
+function plotArray(channel, str) {
 	var arr;
+	var timeOffset;
+	var sampleTime;
+	var measTimeLastSample;
+
 	var arr2 = str.split("\n");
 	var nrPoints = arr2.length - 1;
 	if (nrPoints > 0) {
+		arr = arr2[nrPoints - 1].split(",");
+		measTimeLastSample = arr[0];  // can be unadjusted time in sec units
+		//	document.getElementById('valueDisplay').innerHTML = arr[1] + " " + arr[2]; // value of last measurement
+		var sec = Date.now();//  / 1000;  // mseconds since 1-1-1970 
+		timeOffset = sec - parseFloat(measTimeLastSample) * 1000;
 		for (var p = 0; p < nrPoints; p++) {
 			arr = arr2[p].split(",");
-			if (arr.length >= NRItems) {
-				for (var m = 1; m < NRItems; m++) { // time not used for now
-					if (chartSeries[m] != -1)
-						plot(chartSeries[m], arr[m]);
+			if (arr.length >= NRFields) {
+				for (var m = 1; m < NRSensors; m++) { // time not used for now
+					if (chartSeries[m] != -1) {
+						sampleTime = parseFloat(arr[0]) * 1000 + timeOffset;
+						plot(chartSeries[m], arr[1], arr[2], arr[3], sampleTime);
+					}
 				}
 			}
 		}
-		if (nrPoints == 1) { // then single point added 
-			updateLastDayTimeLabel(data);
-		}
-		else {
-			updateAllDayTimeLabels(data);
-		}
-		chart.draw(data, chartOptions);
+		tRHchart.draw(tempAndRHdata, tempAndRHoptions);
+		CO2chart.draw(CO2Data, CO2Options);
 	}
 }
 
@@ -231,131 +200,57 @@ function timer() {
 	var arr;
 	var arr2;
 	var str;
-	var chargeInfoTbl;
+
 	if (SIMULATE) {
 		simplot();
 	}
 	else {
-		if (firstRequest) {
-			arr = getItem("getLogMeasValues");
-			plotArray(arr);
-			firstRequest = false;
+		// if (firstRequest) {
+		// 	arr = getItem("getLogMeasValues");
+		// 	tempAndRHdata.removeRows(0, tempAndRHdata.getNumberOfRows());
+		// 	CO2Data.removeRows(0, CO2Data.getNumberOfRows());
+		// 	plotArray(arr);
+		// 	firstRequest = false;
+		// }
 
-			arr = getItem("getFunction");  // set function RB
-
-			var rb = document.getElementsByClassName('rw-rb-state');
-			for (var i = 0; i < rb.length; i++) {
-				if (arr == rb[i].value)
-					rb[i].checked = true;
-			}
-		}
-		chargeInfoTbl = document.getElementById("chargeInfoTable");
-		str = getItem("getChargeValues");
-		arr = str.split("\n");
-		for (var colls = 0; colls < arr.length-1; colls++) {  
-			arr2 = arr[colls].split(",");
-			for (var m = 0; m < arr2.length; m++)
-				chargeInfoTbl.rows[m + 1].cells[colls + 1].innerHTML = arr2[m];
-		}
-		presc--;
-
-		if (presc == 0) {
-			presc = REQINTERVAL;
-			str = getItem("getRTMeasValues");
-			arr = str.split(",");
-			// print RT values xx
-			if (arr.length >= NRItems) {
-				if (arr[0] > 0) {
-					if (arr[0] != lastTimeStamp) {
-						lastTimeStamp = arr[0];
-						for (var m = 1; m < NRItems; m++) { // time not used for now 
-							if (chartSeries[m] != -1)
-								plot(chartSeries[m], arr[m]);
+		for (var m = 1; m < NRSensors; m++) {
+			if (chartSeries[m] != -1) {
+				str = getItem("getRTMeasValues" + chartSeries[m].toString()); // request data from enabled sensors
+				arr = str.split(",");
+				if (arr.length >= 3) {
+					if (arr[0] > 0) {
+						if (arr[0] != lastTimeStamp) {
+							lastTimeStamp = arr[0];
+							for (var m = 1; m < 4; m++) { // time not used for now 
+								var value = parseFloat(arr[m]); // from string to float
+//								document.getElementById(displayNames[m]).innerHTML = arr[m] + unit[m];
+							}
+							var sampleTime = Date.now();
+							plot(chartSeries[m], arr[1],arr[2],arr[3], sampleTime);
+							tRHchart.draw(tempAndRHdata, tempAndRHoptions);
+							CO2chart.draw(CO2Data, CO2Options);
 						}
-						updateLastDayTimeLabel(data);
-						chart.draw(data, chartOptions);
 					}
 				}
 			}
-
 		}
 	}
 }
 
-function functionRbClick(val) {
-	sendItem("setFunction=" + val);
-}
-
-function startStop() {
-	halt = !halt;
-	if (halt)
-		document.getElementById('startStopButton').innerHTML = 'start';
-	else
-		document.getElementById('startStopButton').innerHTML = 'stop';
+function clearChart() {
+	tempAndRHdata.removeRows(0, tempAndRHdata.getNumberOfRows());
+	CO2Data.removeRows(0, CO2Data.getNumberOfRows());
+	tRHchart.draw(tempAndRHdata, tempAndRHoptions);
+	CO2chart.draw(CO2Data, CO2Options);
 }
 
 function clearLog() {
-	sendItem("clearLog=1");
-	clear();
+	sendItem("clearLog");
+	clearChart();
 }
 
-function clearChart() {
-	clear();
-}
 function refreshChart() {
 	data.removeRows(0, data.getNumberOfRows());
 	arr = getItem("getLogMeasValues");
 	plotArray(arr);
-}
-
-
-function makeChargeInfoTable() {
-	var str;
-	if (SIMULATE) {
-		str = "Meting,xActueel,xOffset,xx\n rij1, rij2 , rij3\n";
-
-	}
-	else {
-		str = getItem("getChargeTable");
-	}
-	chargeInfoTbl = makeTable("chargeInfoTable", str);
-}
-
-function makeTable(tableID, descriptorData) {
-
-	var tableName = document.getElementById(tableID);
-	var x = tableName.rows.length
-	for (var r = 0; r < x; r++) {
-		tableName.deleteRow(-1);
-	}
-	tblBody = document.createElement("tbody");
-	var lines = descriptorData.split("\n");
-
-	var colls = lines[0].split(",");
-	var rows = lines[1].split(",");
-
-	for (var i = 0; i < rows.length + 1; i++) {
-		var row = document.createElement("tr");
-		for (var j = 0; j < colls.length; j++) {
-			if (i == 0) {
-				var cell = document.createElement("th");
-				var cellText = document.createTextNode(colls[j]);
-				cell.appendChild(cellText);
-			}
-			else {
-				if (j == 0) {
-					var cell = document.createElement("th");
-					var cellText = document.createTextNode(rows[i - 1]);
-					cell.appendChild(cellText);
-				}
-				else {
-					var cell = document.createElement("td");
-				}
-			}
-			row.appendChild(cell);
-		}
-		tblBody.appendChild(row);
-	}
-	tableName.appendChild(tblBody);
-	return tableName;
 }
