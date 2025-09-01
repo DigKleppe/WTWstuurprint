e378fe67b068efb8e21d80c01345a021677175e6
DigKleppe <Digkleppe@gmail.com>
2025-09-01 15:42:34 +0200

    storageversion updated

--- main/include/softwareVersions.h
+++ main/include/softwareVersions.h
@@ -4,6 +4,6 @@
 //#define FIRMWARE_VERSION "0.1.0"
 
 #define FIRMWARE_VERSION "0.1.1"  // make it equal to firmWareVersion.txt 
-#define SPIFFS_VERSION "0.1.0"  
+#define SPIFFS_VERSION "0.1.1"  
 
 #endif /* SOFTWAREVERSIONS_H_ */
